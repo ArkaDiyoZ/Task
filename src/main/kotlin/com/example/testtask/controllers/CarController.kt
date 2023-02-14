535cf264d50fc3ea3cafece3785cb462b19e2446
Arkadii <76054847+ArkaDiyoZ@users.noreply.github.com>
2023-02-15 00:56:21 +0300

    Get request to take a car

--- src/main/kotlin/com/example/testtask/controllers/CarController.kt
+++ src/main/kotlin/com/example/testtask/controllers/CarController.kt
@@ -1,43 +1,40 @@
 package com.example.testtask.controllers
 
 import com.tej.JooQDemo.jooq.sample.model.Tables
-import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Cars
-import com.tej.JooQDemo.jooq.sample.model.tables.records.CarsRecord
+import jakarta.servlet.http.HttpServletResponse
+import jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND
 import org.jooq.DSLContext
-import org.jooq.Record
-import org.jooq.Result
-import org.springframework.beans.factory.annotation.Autowired
+import org.jooq.impl.DSL
+import org.jooq.impl.QOM
+import org.jooq.impl.QOM.JSONObject
 import org.springframework.web.bind.annotation.*
-import java.awt.print.Book
 
 
 @RestController
 @RequestMapping("/cars")
-class BookController(
-) {
-
-    init {
-        println("CONTROLL")
-    }
-    @Autowired
-    val context: DSLContext? = null
-
-    @PostMapping
-    fun postBook(@RequestBody car: Cars) {
-        println(car)
-        println(context)
-        context
-            ?.insertInto(Tables.CARS, Tables.CARS.CAR_GOS_NUM)
-            ?.values(car.carGosNum)
-            ?.execute()
-    }
+class CarController(private val DSLContext: DSLContext) {
 
     @GetMapping("/{id}")
-    fun getCarInfo(): MutableList<Cars>? {
-        return context
-            ?.selectFrom(Tables.CARS)
-            ?.fetchInto(Cars::class.java)
+    fun getCarInfo(@PathVariable("id") id: Int, response: HttpServletResponse): Any? {
+        try {
+            return DSLContext
+            .select()
+            .from(Tables.CARS)
+            .innerJoin(Tables.TYPES)
+            .on(Tables.TYPES.TYPE_ID.eq(Tables.CARS.TYPE_ID))
+            .innerJoin(Tables.MARKS)
+            .on(Tables.CARS.MARK_ID.eq(Tables.MARKS.MARK_ID))
+            .innerJoin(Tables.MODELS)
+            .on(Tables.MARKS.MODEL_ID.eq(Tables.MODELS.MODEL_ID))
+            .where(Tables.CARS.CAR_ID.eq(id))
+            .fetchInto(CarDto::class.java)[0]
+        } catch (e: Exception){
+            if (Exception::class.java.isAssignableFrom(e::class.java)){
+                response.status = SC_NOT_FOUND
+                return "Not Found"
+            }
+        }
+        return null
     }
-
 
 }
